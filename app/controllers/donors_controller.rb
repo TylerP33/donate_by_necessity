5da56a3b4b093c56b18843c65cc61a21ebf6d89c
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-04-30 16:10:22 -0500

    Donor controller
    Set up donor controller
    
    session controller will be set up next to make sure code is executed properly

--- app/controllers/donors_controller.rb
+++ app/controllers/donors_controller.rb
@@ -1,2 +1,17 @@
 class DonorsController < ApplicationController
+	def new
+		@donor = Donor.new
+	end
+
+	def create
+	@donor = Donor.new(donor_params)
+	if @donor.save
+		session[:donor_id] = @donor.id
+		redirect_to root_path
+	else
+		render :new
+	end
+
+	private donor_params
+		params.require(:donor).permit(:email, :password)
 end
