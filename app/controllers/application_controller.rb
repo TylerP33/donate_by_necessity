7bd6e3eb3a933b1985382dd878dfebbf68cd862b
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-05-01 10:37:16 -0500

    Added necessary helper methods
    Added necessary helper methods in Application Controller regarding loggin in.
    
    I did this because I want to make sure every bit of the OMNIauth is working correctly.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+
+  protect_from_forgery with: :exception
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= Donor.find(session[:user_id]) if session[:user_id]
+  end
+
+  def redirect_if_not_logged_in
+   	if !logged_in?
+    	redirect_to login_path, alert: "Please login to access your donor pag"
+  end
+
+  def logged_in?
+    !!current_user                #
+  end
+
 end
