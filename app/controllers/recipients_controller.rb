a1ca087a32c51500a22fb7eb5c7e604a1267699f
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-05-01 14:30:47 -0500

    Added Recipients Controller Logic/Views
    Added the ability to create a recipient.
    
    Now, need to somehow nest these forms, and be able to put what donation categories I want in the checkbox for the form_for recipient

--- app/controllers/recipients_controller.rb
+++ app/controllers/recipients_controller.rb
@@ -1,2 +1,40 @@
 class RecipientsController < ApplicationController
+
+	def index
+		@recipients = Recipient.all
+
+	def new
+		@recipient = Recipient.new
+	end 
+
+	def edit
+
+	end 
+
+	def create
+		@recipient = Recipient.new(recipient_params)
+		respond_to do |format|
+			if @recipient.save
+				format.html {redirect_to @recipient, notice: 'New Recipient was succesfully created'}
+			else
+				format.html {render :new}
+			end
+		end
+	end 
+
+
+	def recipient_params
+		params.require(:recipient).permit(
+			:first_name,
+			:last_name,
+			:location,
+			:gender,
+			:personal_notes,
+			:urgent_needs
+			)
+	end
+
+
+end
+
 end
