b420c53e13c63622413e392fab67a7e5cf509400
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-05-01 12:38:20 -0500

    Added local signup logic
    Needed the ability to have a local login, as well as oauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,22 @@
 class SessionsController < ApplicationController
 
 	def new
-
 	end 
 	
 	def create
-		donor = Donor.find_or_create_by_omniauth(request.env["omniauth.auth"])
-    	session[:user_id] = donor.id
-    	redirect_to root_path
+		if auth_hash = request.env["omniauth.auth"]
+			donor = Donor.find_or_create_by_omniauth(request.env["omniauth.auth"])
+    		session[:user_id] = donor.id
+    		redirect_to root_path
+    	else
+    	donor = Donor.find_by(email: params[:email])
+      	if donor && donor.authenticate(params[:password])
+        	session[:user_id] = donor.id
+        	redirect_to root_path
+      	else
+        	render "sessions/new"
+     	 end
+       end
   	end
 
 	def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,15 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
+  get '/signin', to: 'sessions#new', as: 'signin'
+  post '/session', to: 'sessions#create', as: 'session'
 
-  resources :sessions, only: [:create, :destroy]
+
+  resources :sessions, only: [:new, :create, :destroy]
   resource :home, only: [:show]
 
   root to: "home#show"
 
-  resources :recipients
-  resources :donors only: [:new, :create]
-  resources :categories
+  resources :donors, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
