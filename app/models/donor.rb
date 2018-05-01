c8007680b2e3f8931f8d19ba5b1889e024aa65a2
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-05-01 10:19:34 -0500

    Donor model logic - OMNiauth
    Added the necessary logic for Oauth in Donor model

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -9,4 +9,16 @@ class Donor < ApplicationRecord
 
 	has_many :recipients
 	has_many :categories, through: :recipients
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |donor|
+      donor.provider = auth.provider
+      donor.uid = auth.uid
+      donor.name = auth.info.name
+      donor.oauth_token = auth.credentials.token
+      donor.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      donor.save!
+    end
+  end
+
 end
