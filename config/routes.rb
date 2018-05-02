103f3beca03bb5dc17fce0fb4a8b125361e10a6a
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-05-02 11:45:36 -0500

    Added nested route - also added custom route
    donor_interface is going to be where a donor keeps track of everything.
    
    This is a custom route that is crucial to my app

--- config/routes.rb
+++ config/routes.rb
@@ -4,19 +4,19 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
-
-  get '/signin', to: 'sessions#new', as: 'signin'
-  post '/session', to: 'sessions#create', as: 'session'
-  get '/signup', to: 'donors#new', as: 'signup'
-  post '/donor', to: 'donors#create', as: "donor"
-  get '/welcome_donor', to: 'donors#index', as: "welcome_donor"
-
+  get 'donor_home', to: "donors#donor_home", as: "donor_interface"
 
   resources :sessions, only: [:new, :create, :destroy]
   resource :home, only: [:show]
-
   root to: "home#show"
 
-  resources :donors, only: [:index, :new, :create]
+  resources :donors
+  resources :recipients
+
+  resources :categories do
+    resources :recipients
+  end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
