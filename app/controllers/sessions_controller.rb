294ddbe2cb7f6097d31863919ab0321c34600ed9
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-05-09 16:45:30 -0500

    Fixed pathing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,15 +13,15 @@ class SessionsController < ApplicationController
       	if donor && donor.authenticate(params[:password])
         	session[:user_id] = donor.id
         	redirect_to donor_interface_path
-      	else;
-        	redirect_to donor_interface_path
+      	else
+        	render "sessions/new"
      	 end
        end
   	end
 
 	def destroy
     	session[:user_id] = nil
-    	redirect_to root_path
+    	redirect_to home_show_path
 	end
 
 end 
