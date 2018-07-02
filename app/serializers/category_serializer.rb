bf2d1525bc0a5bca10c028a5176d41067cf759fd
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-07-02 15:39:20 -0500

    Fix spacing

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,5 +1,3 @@
 class CategorySerializer < ActiveModel::Serializer
   attributes :id, :toilet_paper, :diapers, :dental_hygiene, :first_aid, :general_hygiene, :underwear_socks, :blankets, :school_supplies
-
-
 end
