0e3575af0d1d66bb7e0b85fc883520819ea88541
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-07-05 16:03:55 -0500

    Fix class name

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-  $(".js-more").one('click', function() {
+  $(".js-all-donations").one('click', function() {
     $.get("/categories.json", function(category) {
         category.forEach(function (cat) {
            const html = "<tr><td>"+cat.id+"</td><td>"+"$"+cat.toilet_paper+"</td><td>"+"$"+cat.diapers+"</td><td>"+"$"+cat.dental_hygiene+"</td><td>"+"$"+cat.first_aid+"</td><td>"+"$"+cat.general_hygiene+"</td><td>"+"$"+cat.underwear_socks+"</td><td>"+"$"+cat.blankets+"</td><td>"+"$"+cat.school_supplies+"</td></tr>"
