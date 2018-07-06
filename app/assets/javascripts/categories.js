9017024f4259491e99959c2c41fc9de9e0a77d54
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-07-06 17:33:40 -0500

    Comsetic changes - fixed function block spacing

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -3,13 +3,23 @@ $(document).ready(function () {
   $(".js-all-donations").one('click', function() {
     $.get("/categories.json", function(category) {
         category.forEach(function (cat) {
-           const html = "<tr><td>"+cat.id+"</td><td>"+"$"+cat.toilet_paper+"</td><td>"+"$"+cat.diapers+"</td><td>"+"$"+cat.dental_hygiene+"</td><td>"+"$"+cat.first_aid+"</td><td>"+"$"+cat.general_hygiene+"</td><td>"+"$"+cat.underwear_socks+"</td><td>"+"$"+cat.blankets+"</td><td>"+"$"+cat.school_supplies+"</td></tr>"
+           const html = 
+
+           "<tr><td>" + cat.id+
+           "</td><td>" + "$"+cat.toilet_paper+
+           "</td><td>" + "$"+cat.diapers+
+           "</td><td>" + "$"+cat.dental_hygiene+
+           "</td><td>" + "$"+cat.first_aid+
+           "</td><td>" + "$"+cat.general_hygiene+
+           "</td><td>" + "$"+cat.underwear_socks+
+           "</td><td>" + "$"+cat.blankets+
+           "</td><td>" + "$"+cat.school_supplies+
+           "</td></tr>"
+
            $("#totalDonationTable").find('tbody').append(html);
             });
         });
     });
-
-  
 });
 
 
--- app/assets/javascripts/recipients.js
+++ app/assets/javascripts/recipients.js
@@ -7,9 +7,9 @@ $(document).ready(function () {
     	$("#firstName").text(recipient["first_name"]);
     	$("#lastName").text(recipient["last_name"]);
     	$("#location").text(recipient["location"]);
-        $("#gender").text(recipient["gender"]);
-        $("#personalNotes").text(recipient["personal_notes"]);
-        $(".js-next-recipient").attr("data-id", recipient["id"]);
+      $("#gender").text(recipient["gender"]);
+      $("#personalNotes").text(recipient["personal_notes"]);
+      $(".js-next-recipient").attr("data-id", recipient["id"]);
         });
     });
 
@@ -18,10 +18,22 @@ $(document).ready(function () {
     const categoryId = parseInt($(this).attr("data-id"))
     console.log(categoryId);
      $.get("/recipients/"+categoryId+".json", function(recipient) {
-           const html = "<tr><td>"+recipient["category"]["id"]+"</td><td>"+"$"+recipient["category"]["toilet_paper"]+"</td><td>"+"$"+recipient["category"]["diapers"]+"</td><td>"+"$"+recipient["category"]["dental_hygiene"]+"</td><td>"+"$"+recipient["category"]["first_aid"]+"</td><td>"+"$"+recipient["category"]["general_hygiene"]+"</td><td>"+"$"+recipient["category"]["underwear_socks"]+"</td><td>"+"$"+recipient["category"]["blankets"]+"</td><td>"+"$"+recipient["category"]["school_supplies"]+"</td></tr>"
-           $("#recipientDonations").find('tbody').append(html);
-            });
+      const html = 
+
+      "<tr><td>" + recipient["category"]["id"]+
+      "</td><td>" + "$" +recipient["category"]["toilet_paper"]+
+      "</td><td>" + "$" +recipient["category"]["diapers"]+
+      "</td><td>" + "$" +recipient["category"]["dental_hygiene"]+
+      "</td><td>" + "$" +recipient["category"]["first_aid"]+
+      "</td><td>" + "$" +recipient["category"]["general_hygiene"]+
+      "</td><td>" + "$" +recipient["category"]["underwear_socks"]+
+      "</td><td>" + "$" +recipient["category"]["blankets"]+
+      "</td><td>" + "$" +recipient["category"]["school_supplies"]+
+      "</td></tr>"
+
+      $("#recipientDonations").find('tbody').append(html);
         });
     });
+});
 
 
