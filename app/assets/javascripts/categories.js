f5383220436623a43705fc9dcf36e26cad370db1
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-07-06 17:38:10 -0500

    Fix spacing

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -5,15 +5,15 @@ $(document).ready(function () {
         category.forEach(function (cat) {
            const html = 
 
-           "<tr><td>" + cat.id+
-           "</td><td>" + "$"+cat.toilet_paper+
-           "</td><td>" + "$"+cat.diapers+
-           "</td><td>" + "$"+cat.dental_hygiene+
-           "</td><td>" + "$"+cat.first_aid+
-           "</td><td>" + "$"+cat.general_hygiene+
-           "</td><td>" + "$"+cat.underwear_socks+
-           "</td><td>" + "$"+cat.blankets+
-           "</td><td>" + "$"+cat.school_supplies+
+           "<tr><td>" + cat.id +
+           "</td><td>" + "$" + cat.toilet_paper +
+           "</td><td>" + "$" + cat.diapers +
+           "</td><td>" + "$" + cat.dental_hygiene +
+           "</td><td>" + "$" + cat.first_aid +
+           "</td><td>" + "$" + cat.general_hygiene +
+           "</td><td>" + "$" + cat.underwear_socks +
+           "</td><td>" + "$" + cat.blankets +
+           "</td><td>" + "$" + cat.school_supplies +
            "</td></tr>"
 
            $("#totalDonationTable").find('tbody').append(html);
--- app/assets/javascripts/recipients.js
+++ app/assets/javascripts/recipients.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   $(".js-next-recipient").click(function() {
   	const recipientId = parseInt($(this).attr("data-id"));
   	console.log(recipientId);
-    $.get("/recipients/"+recipientId+"/next", function(recipient) {
+    $.get("/recipients/" + recipientId + "/next", function(recipient) {
     	$("#firstName").text(recipient["first_name"]);
     	$("#lastName").text(recipient["last_name"]);
     	$("#location").text(recipient["location"]);
@@ -17,18 +17,18 @@ $(document).ready(function () {
   $(".js-show-category").one('click', function() {
     const categoryId = parseInt($(this).attr("data-id"))
     console.log(categoryId);
-     $.get("/recipients/"+categoryId+".json", function(recipient) {
+     $.get("/recipients/" + categoryId + ".json", function(recipient) {
       const html = 
 
-      "<tr><td>" + recipient["category"]["id"]+
-      "</td><td>" + "$" +recipient["category"]["toilet_paper"]+
-      "</td><td>" + "$" +recipient["category"]["diapers"]+
-      "</td><td>" + "$" +recipient["category"]["dental_hygiene"]+
-      "</td><td>" + "$" +recipient["category"]["first_aid"]+
-      "</td><td>" + "$" +recipient["category"]["general_hygiene"]+
-      "</td><td>" + "$" +recipient["category"]["underwear_socks"]+
-      "</td><td>" + "$" +recipient["category"]["blankets"]+
-      "</td><td>" + "$" +recipient["category"]["school_supplies"]+
+      "<tr><td>" + recipient["category"]["id"] +
+      "</td><td>" + "$" + recipient["category"]["toilet_paper"] +
+      "</td><td>" + "$" + recipient["category"]["diapers"] +
+      "</td><td>" + "$" + recipient["category"]["dental_hygiene"] +
+      "</td><td>" + "$" + recipient["category"]["first_aid"] +
+      "</td><td>" + "$" + recipient["category"]["general_hygiene"] +
+      "</td><td>" + "$" + recipient["category"]["underwear_socks"] +
+      "</td><td>" + "$" + recipient["category"]["blankets"] +
+      "</td><td>" + "$" + recipient["category"]["school_supplies"] +
       "</td></tr>"
 
       $("#recipientDonations").find('tbody').append(html);
