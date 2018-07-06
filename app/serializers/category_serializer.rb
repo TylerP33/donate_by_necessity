0b78cb2c45cc31bc260d35005a876de9f23652f0
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-07-06 15:13:33 -0500

    Add AR associations to serializers

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,5 +1,7 @@
 class CategorySerializer < ActiveModel::Serializer
   attributes :id, :toilet_paper, :diapers, :dental_hygiene, :first_aid, :general_hygiene, :underwear_socks, :blankets, :school_supplies
+  has_many :recipients
+  
 
   def serializable_hash(adapter_options = nil, options = {}, adapter_instance = self.class.serialization_adapter_instance)
   	hash = super
--- app/serializers/recipient_serializer.rb
+++ app/serializers/recipient_serializer.rb
@@ -1,3 +1,4 @@
 class RecipientSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :location, :gender, :personal_notes
+  belongs_to :category
 end
