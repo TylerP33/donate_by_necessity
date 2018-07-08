08c4f3450c4b721d7a8115ed446a7483e1ef6b0f
Tyler Pulse <32463199+TylerP33@users.noreply.github.com>
2018-07-08 12:53:12 -0500

    Add JSMO functionality
    - add RecipientCategory class - fulfills has_many project requirement
    - added formatRecipientsCategory to format HTML

--- app/assets/javascripts/recipients.js
+++ app/assets/javascripts/recipients.js
@@ -11,36 +11,50 @@ $(document).ready(function () {
       $(".js-next-recipient").attr("data-id", recipient["id"]);
         });
     });
+  
 
-
-  $(".js-show-category").one('click', function() {
+    $(".js-show-category").one('click', function() {
     const categoryId = parseInt($(this).attr("data-id"));
-     $.get("/recipients/" + categoryId + ".json", function(recipient) {
-      const html = 
-
-      "<tr><td>" + recipient["category"]["id"] +
-      "</td><td>" + "$" + recipient["category"]["toilet_paper"] +
-      "</td><td>" + "$" + recipient["category"]["diapers"] +
-      "</td><td>" + "$" + recipient["category"]["dental_hygiene"] +
-      "</td><td>" + "$" + recipient["category"]["first_aid"] +
-      "</td><td>" + "$" + recipient["category"]["general_hygiene"] +
-      "</td><td>" + "$" + recipient["category"]["underwear_socks"] +
-      "</td><td>" + "$" + recipient["category"]["blankets"] +
-      "</td><td>" + "$" + recipient["category"]["school_supplies"] +
-      "</td></tr>"
-
-      $("#recipientDonations").find('tbody').append(html);
+     $.get("/recipients/" + categoryId + ".json", function(rec) {
+      const recipient = new RecipientCategory(rec)
+      const recHTML =  recipient.formatRecipientsCategory()
+      $("#recipientDonations").find('tbody').append(recHTML);
         });
     });
 
-    function Recipient(recipient) {
-      this.id = recipient.id
-      this.first_name = recipient.first_name
-      this.last_name = recipient.last_name
-      this.location = recipient.location
-      this.gender = recipient.gender
-      this.personal_notes = recipient.personal_notes
+  function RecipientCategory(recipient) {
+      this.id = recipient["category"]["id"]
+      this.toilet_paper = recipient["category"]["toilet_paper"]
+      this.diapers = recipient["category"]["diapers"]
+      this.dental_hygiene = recipient["category"]["dental_hygiene"]
+      this.first_aid = recipient["category"]["first_aid"]
+      this.general_hygiene = recipient["category"]["general_hygiene"]
+      this.underwear_socks = recipient["category"]["underwear_socks"]
+      this.blankets = recipient["category"]["blankets"]
+      this.school_supplies = recipient["category"]["school_supplies"]
     }
+
+
+
+    RecipientCategory.prototype.formatRecipientsCategory = function () {
+      const html = 
+
+      "<tr><td>" + this.id +
+      "</td><td>" + "$" + this.toilet_paper +
+      "</td><td>" + "$" + this.diapers +
+      "</td><td>" + "$" + this.dental_hygiene +
+      "</td><td>" + "$" + this.first_aid +
+      "</td><td>" + "$" + this.general_hygiene +
+      "</td><td>" + "$" + this.underwear_socks +
+      "</td><td>" + "$" + this.blankets +
+      "</td><td>" + "$" + this.school_supplies +
+      "</td></tr>"
+
+      return html
+
+    }
+
+
 
 });
 
